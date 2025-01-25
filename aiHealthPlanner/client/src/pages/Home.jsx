87b8919871e230bc49bcc21253560cccd24cf062
Shubham Kadariya <shubham.kadariya021@apexcollege.edu.np>
2025-01-25 21:12:13 +0545

    Final Prompt

--- aiHealthPlanner/client/src/pages/Home.jsx
+++ aiHealthPlanner/client/src/pages/Home.jsx
@@ -7,10 +7,10 @@ const Home = () => {
       <Hero />
       <div className="mt-2 mb-2 w-[98%] bg-transparent flex flex-col justify-start items-center lg:flex-row lg:justify-around lg:items-center">
         <div className="w-[95%] lg:w-[30%] flex flex-col justify-center items-center mb-[2rem] lg:mb-0 mt-[2rem] lg:ml-[2rem]">
-          <div className="text-[1.5rem] font-bold mb-2 text-[#FF6F00]">
-            Your Wellness, Your Way
+          <div className="text-[2.25rem] font-bold mb-2 text-[#FF6F00]">
+            <span className="text-black"> Your Wellness, </span> Your Way
           </div>
-          <div className="text-[1.10rem] mt-2 text-justify">
+          <div className="text-[1.20rem] mt-2 text-justify">
             ArogyaX is a wellness app that offers tailored recommendations and
             plans for 50 common diseases. Based on your budget, group size,
             duration, and location preferences, it provides personalized
